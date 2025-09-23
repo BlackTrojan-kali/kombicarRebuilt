996d10bb0d3234d8ec08cdcd1c9558dd1527d2e1
blacktrojan <chendjousil@gmail.com>
2025-09-23 15:49:37 +0100

    Message décrivant mes changements

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -10,7 +10,7 @@ export const authContext = createContext({});
 export function AuthContextProvider({ children }) {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-    const API_URL = "https://test.kombicar.ap
+    const API_URL = "https://test.kombicar.api"
     const logout = async (showToast = true) => {
         setLoading(true);
         localStorage.removeItem('accessToken');
